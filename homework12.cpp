f56b42e8d92bcbc80d89ad0f0921c7c5452964c6
Egor <145016046+Infinity164@users.noreply.github.com>
2024-03-04 19:18:48 +0300

    Add files via upload

---
+++ homework12.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+#define random(a, b) a+rand()%(b-a+1)
+
+int task1(int degree, int index) {
+    int answer = degree;
+    for (int i = 1; i < index; i++) {
+        answer *= degree;
+    }
+    return answer;
+}
+
+
+int task2(int min, int max) {
+    int summ = 0;
+    for (int i = min; i <= max; i++) {
+        summ += i;
+    }
+    return summ;
+}
+
+int main()
+{
+
+}
