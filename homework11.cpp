b46b561cd3bb9d79adb43c7b3ffe8d249a300216
Egor <145016046+Infinity164@users.noreply.github.com>
2024-03-04 19:14:54 +0300

    Add files via upload

---
+++ homework11.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+using namespace std;
+
+#define random(a, b) a+rand()%(b-a+1)
+
+bool flag = false;
+void fullArrayByAnArray(int array[], int array2[]) {
+    for (int i = 0, k = 0; i < 9; i++) {
+        for (int j = 9; j > i; j--) {
+            if (array[i] == array[j]) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            array2[k++] = array[i];
+            flag = false;
+        }
+    }
+}
+
+void fillArray(int array[]) {
+    for (int i = 0; i < 10; i++) {
+        array[i] = random(1, 10);
+    }
+}
+
+void showArray(int array[]) {
+    for (int i = 0; i < 10; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    srand(time(NULL));
+    int array[10] = {};
+    int array2[10] = {};
+    fillArray(array);
+    showArray(array);
+    fullArrayByAnArray(array, array2);
+    showArray(array2);
+}
+
